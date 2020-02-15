ebbcb116d69d22f8933ea353979c53fc8675dec9
blaueQuelle <jk@blauequelle.de>
2020-02-15 18:49:41 +0100

    Config Options updated, added cookie groups, show only options for selected groups

--- PrivacyWireBanner.php
+++ PrivacyWireBanner.php
@@ -2,31 +2,33 @@
     /**
      * @var PrivacyWire $module Instance of the PrivacyWire module;
      */
-    $cookieOptions = [
-        'necessary'
-    ];
 
-    if(!empty($module->cookies_statistics)) {
-        $cookieOptions[] = 'statistics';
-    }
+    $showAllButton = (in_array("all", $module->cookie_groups));
+    $showNecessaryButton = (in_array("necessary", $module->cookie_groups));
 
-    if(!empty($module->cookies_external_media)) {
-        $cookieOptions[] = 'external_media';
-    }
-    if(!empty($module->cookies_marketing)) {
-        $cookieOptions[] = 'marketing';
-    }
+    $showStatisticsButton = (in_array("statistics", $module->cookie_groups));
+    $showMarketingButton = (in_array("marketing", $module->cookie_groups));
+    $showExternalMediaButton = (in_array("external_media", $module->cookie_groups));
+
+    $showChooseButton = (
+        $showStatisticsButton ||
+        $showMarketingButton ||
+        $showExternalMediaButton
+    );
 ?>
 
 <div class="privacywire-wrapper">
     <div class="privacywire privacywire-banner">
-        <header class="privacywire-header"> <?php echo $module->content_banner_title;?></header>
+        <?php if (!empty($module->content_banner_title)): ?>
+            <header class="privacywire-header"> <?php echo $module->content_banner_title; ?></header>
+        <?php endif; ?>
         <div class="privacywire-body">
-            <div class="privacywire-text"><?php echo $module->content_banner_text;?></div>
+            <div class="privacywire-text"><?php echo $module->content_banner_text; ?></div>
             <div class="privacywire-buttons">
-                <button class="allow-all"><?php echo $module->content_banner_button_allow_all;?></button>
-                <button class="allow-necessary"><?php echo $module->content_banner_button_allow_necessary;?></button>
-                <button class="choose"><?php echo $module->content_banner_button_choose;?></button>
+                <button class="allow-all" <?php echo (!$showAllButton) ? "hidden" : ""; ?>><?php echo $module->content_banner_button_allow_all; ?></button>
+                <button class="allow-necessary" <?php echo (!$showNecessaryButton) ? "hidden" : ""; ?>><?php echo $module->content_banner_button_allow_necessary; ?></button>
+
+                <button class="choose" <?php echo (!$showChooseButton) ? "hidden" : ""; ?>><?php echo $module->content_banner_button_choose; ?></button>
             </div>
         </div>
 
@@ -35,20 +37,61 @@
         <header class="privacywire-header"></header>
         <div class="privacywire-body">
             <div class="privacywire-text">
-                    <ul>
-                        <li><label for="necessary"><input class="required" type="checkbox" name="necessary" id="necessary" value="1" checked disabled>Necessary</label></li>
-                        <li><label for="statistics"><input class="optional" type="checkbox" name="statistics" id="statistics" value="0">Statistics</label></li>
-                        <li><label for="external_media"><input class="optional" type="checkbox" name="external_media" id="external_media" value="0">External Media</label></li>
-                        <li><label for="marketing"><input class="optional" type="checkbox" name="marketing" id="marketing" value="0" >Marketing</label></li>
-                    </ul>
+                <ul>
+                    <li <?php echo (!$showNecessaryButton) ? "hidden" : ""; ?>>
+                        <label for="necessary">
+                            <input class="required"
+                                   type="checkbox"
+                                   name="necessary"
+                                   id="necessary"
+                                   value="1"
+                                   checked
+                                   disabled>
+                            <?php echo $module->cookies_necessary_label; ?>
+                        </label>
+                    </li>
+
+                    <li <?php echo (!$showStatisticsButton) ? "hidden" : ""; ?>>
+                        <label for="statistics">
+                            <input class="optional"
+                                   type="checkbox"
+                                   name="statistics"
+                                   id="statistics"
+                                   value="0">
+                            <?php echo $module->cookies_statistics_label; ?>
+                        </label>
+                    </li>
+
+                    <li <?php echo (!$showMarketingButton) ? "hidden" : ""; ?>>
+                        <label for="marketing">
+                            <input class="optional"
+                                   type="checkbox"
+                                   name="marketing"
+                                   id="marketing"
+                                   value="0">
+                            <?php echo $module->cookies_marketing_label; ?>
+                        </label>
+                    </li>
+
+                    <li <?php echo (!$showExternalMediaButton) ? "hidden" : ""; ?>>
+                        <label for="external_media">
+                            <input class="optional"
+                                   type="checkbox"
+                                   name="external_media"
+                                   id="external_media"
+                                   value="0">
+                            <?php echo $module->cookies_external_media_label; ?>
+                        </label>
+                    </li>
+                </ul>
             </div>
             <div class="privacywire-buttons">
-                <button class="toggle"><?php echo $module->content_banner_button_toggle;?></button>
-                <button class="save"><?php echo $module->content_banner_button_save;?></button>
+                <button class="toggle"><?php echo $module->content_banner_button_toggle; ?></button>
+                <button class="save"><?php echo $module->content_banner_button_save; ?></button>
             </div>
         </div>
     </div>
     <div class="privacywire privacywire-message">
-        <div class="priavywire-body"><?php echo $module->content_banner_save_message;?></div>
+        <div class="priavywire-body"><?php echo $module->content_banner_save_message; ?></div>
     </div>
 </div>
--- PrivacyWireConfig.php
+++ PrivacyWireConfig.php
@@ -5,12 +5,15 @@ class PrivacyWireConfig extends ModuleConfig
     public function getDefaults()
     {
         return [
-            'enable' => true,
             'version' => 1,
+            'cookie_groups' => [ "all", "necessary" ],
             'respectDNT' => false,
-            'allowOptions' => true,
+            'cookies_necessary_label' => $this->_('Necessary'),
+            'cookies_statistics_label' => $this->_('Statistics'),
+            'cookies_marketing_label' => $this->_('Marketing'),
+            'cookies_external_media_label' => $this->_('External Media'),
             'content_banner_title' => $this->_("This website is using cookies to provide a good browsing experience"),
-            'content_banner_text' => $this->_("These include essential cookies that are necessary for the operation of the site, as well as others that are used only for anonymous statistical purposes, for comfort settings or to display personalized content. You can decide for yourself which categories you want to allow. Please note that based on your settings, not all functionalities of the website may be available."),
+            'content_banner_text' => $this->_("These include essential cookies that are necessary for the operation of the site, as well as others that are used only for anonymous statistical purposes, for comfort settings or to display personalized content. You can decide for yourself which categories you want to allow. Please note that based on your settings, not all functions of the website may be available."),
             'content_banner_privacy_link' => null,
             'content_banner_button_allow_all' => $this->_("Accept all"),
             'content_banner_button_allow_necessary' => $this->_("Accept necessary cookies only"),
@@ -18,10 +21,6 @@ class PrivacyWireConfig extends ModuleConfig
             'content_banner_button_save' => $this->_("Save preferences"),
             'content_banner_button_toggle' => $this->_("Toggle options"),
             'content_banner_save_message' => $this->_("Your cookie preferences have been saved."),
-            'cookies_necessary' => $this->_("wire|domain.tld|This cookie is required for secure login and to detect spam or abuse of this website.|Session \nprivacywire|domain.tld|This cookie saves the consent status for cookies.|1 Year"),
-            'cookies_statistics' => $this->_(""),
-            'cookies_external_media' => $this->_(""),
-            'cookies_marketing' => $this->_(""),
         ];
     }
 
@@ -29,54 +28,91 @@ class PrivacyWireConfig extends ModuleConfig
     {
         $inputfields = parent::getInputfields();
 
-        // 1. Checkbox for enabling or disabling the whole module
-        $f = $this->modules->get('InputfieldCheckbox');
-        $f->attr('name', 'enable');
-        $f->label = $this->_('Enable PrivacyWire and Cookie Banner');
-        $inputfields->add($f);
-
-        // 2. Fieldset for module config
-        $fieldset = $this->modules->get('InputfieldFieldset');
-        $fieldset->label = $this->_("PrivacyWire Settings");
-        $fieldset->showIf = "enable=1";
-        $inputfields->add($fieldset);
-
         // version integer
         $f = $this->modules->get('InputfieldInteger');
         $f->attr('name', 'version');
+        $f->description = $this->_("When you increase the version number, all users have to opt-in again. This version number is saved with the users consent in the cookie.");
         $f->label = $this->_('Version Number');
         $f->attr('min', 1);
         $f->columnWidth = 33;
-        $fieldset->add($f);
+        $inputfields->add($f);
+
+        // opt-in type
+        $f = $this->modules->get('InputfieldAsmSelect');
+        $f->attr('name', 'cookie_groups');
+        $f->description = $this->_("Choose, which groups of cookies the user is allowed to choose. When more than the two default groups (all & necessary) are allowed, the option window will be shown to the user.");
+        $f->label = $this->_('Cookie Groups');
+        $f->options = [
+            "necessary" => $this->_("Necessary Cookies"),
+            "all" => $this->_("All Cookies"),
+            "statistics" => $this->_("Statistics"),
+            'marketing' => $this->_("Marketing"),
+            'external_media' => $this->_("External Media")
+        ];
+        $f->columnWidth = 33;
+        $inputfields->add($f);
 
         // respect "do not track"
         $f = $this->modules->get('InputfieldCheckbox');
         $f->attr('name', 'respectDNT');
+        $f->description = $this->_("When enabled, PrivacyWire checks if the users browser sends the DNT-Header. If so, no cookie banner will be shown and the user will be handled like 'Only Necessary Cookies' would be chosen.");
         $f->label = $this->_('DNT: Do Not Track');
         $f->checkboxLabel = $this->_('Respect "Do Not Track" Settings from the browser');
         $f->columnWidth = 33;
-        $fieldset->add($f);
+        $inputfields->add($f);
 
-        // allow to choose between cookie groups (otherwise only all or necessary)
-        $f = $this->modules->get('InputfieldCheckbox');
-        $f->attr('name', 'allowOptions');
-        $f->label = $this->_('Allow to choose cookie category');
-        $f->checkboxLabel = $this->_('Allow the user to choose, which category of cookies shall be allowed');
-        $f->columnWidth = 33;
-        $fieldset->add($f);
+        // fieldset for cookie groups
+        $cookieFieldset = $this->modules->get('InputfieldFieldset');
+        $cookieFieldset->label = $this->_("Cookie Group Labels");
+        $cookieFieldset->description = $this->_("Label of the cookie groups in 'Choose Cookies' window");
+        $inputfields->add($cookieFieldset);
 
+        // label for cookie group: necessary
+        $f = $this->modules->get('InputfieldText');
+        $f->attr('name', 'cookies_necessary_label');
+        $f->label = $this->_('Necessary Cookies: Label');
+        $f->showIf("cookie_groups=necessary");
+        $f->columnWidth = 25;
+        $cookieFieldset->add($f);
+
+        // label for cookie group: statistics
+        $f = $this->modules->get('InputfieldText');
+        $f->attr('name', 'cookies_statistics_label');
+        $f->label = $this->_('Statistics Cookies: Label');
+        $f->showIf("cookie_groups=statistics");
+        $f->columnWidth = 25;
+        $cookieFieldset->add($f);
+
+        // label for cookie group: marketing
+        $f = $this->modules->get('InputfieldText');
+        $f->attr('name', 'cookies_marketing_label');
+        $f->label = $this->_('Marketing Cookies: Label');
+        $f->showIf("cookie_groups=marketing");
+        $f->columnWidth = 25;
+        $cookieFieldset->add($f);
+
+        // label for cookie group: external media
+        $f = $this->modules->get('InputfieldText');
+        $f->attr('name', 'cookies_external_media_label');
+        $f->label = $this->_('External Media Cookies: Label');
+        $f->showIf("cookie_groups=external_media");
+        $f->columnWidth = 25;
+        $cookieFieldset->add($f);
 
         // fieldset for contents of the module markup
         $content = $this->modules->get('InputfieldFieldset');
         $content->label = $this->_("Banner Content");
-        $fieldset->add($content);
+        $inputfields->add($content);
 
+        // banner headline (optional)
         $f = $this->modules->get('InputfieldText');
         $f->attr('name', 'content_banner_title');
+        $f->description = $this->_("Optional: If empty, no headline will be shown in the banner.");
         $f->label = $this->_('Banner Title');
         $f->columnWidth = 50;
         $content->add($f);
 
+        // banner body copy
         $f = $this->modules->get('InputfieldCKEditor');
         $f->attr('name', 'content_banner_text');
         $f->attr('toolbar', 'Bold, Italic, NumberedList, BulletedList, PWLink, Unlink, PWImage, Table');
@@ -85,76 +121,50 @@ class PrivacyWireConfig extends ModuleConfig
         $f->columnWidth = 50;
         $content->add($f);
 
+        // privacy page
         $f = $this->modules->get('InputfieldPage');
         $f->attr('name', 'content_banner_privacy_link');
+        $f->description = $this->_("If you want to output a link to your privacy policy page, choose the  corresponding page here");
         $f->inputfield = "InputfieldPageListSelect";
         $f->derefAsPage = 1;
         $f->label = $this->_('Privacy Link / Page');
         $f->columnWidth = 100;
         $content->add($f);
 
+        // Button Label: Allow All
         $f = $this->modules->get('InputfieldText');
         $f->attr('name', 'content_banner_button_allow_all');
         $f->label = $this->_('Button Label: Allow All Cookies');
         $f->columnWidth = 25;
         $content->add($f);
 
+        // Button Label: Allow Necessary
         $f = $this->modules->get('InputfieldText');
         $f->attr('name', 'content_banner_button_allow_necessary');
         $f->label = $this->_('Button Label: Allow Necessary Cookies');
         $f->columnWidth = 25;
         $content->add($f);
 
+        // Button Label: Choose Cookies
         $f = $this->modules->get('InputfieldText');
         $f->attr('name', 'content_banner_button_choose');
         $f->label = $this->_('Button Label: Choose Cookies');
         $f->columnWidth = 25;
         $content->add($f);
 
+        // Button Label: Save Preferences
         $f = $this->modules->get('InputfieldText');
         $f->attr('name', 'content_banner_button_save');
         $f->label = $this->_('Button Label: Save Preferences');
         $f->columnWidth = 25;
         $content->add($f);
 
+        // Button Label: Saved Message
         $f = $this->modules->get('InputfieldText');
         $f->attr('name', 'content_banner_save_message');
         $f->label = $this->_('Save Message');
         $f->columnWidth = 100;
         $content->add($f);
-
-        // fieldset for cookie groups and settings
-        $cookieFieldset = $this->modules->get('InputfieldFieldset');
-        $cookieFieldset->label = $this->_("Cookie Settings");
-        $fieldset->add($cookieFieldset);
-
-        $f = $this->modules->get('InputfieldTextarea');
-        $f->attr('name', 'cookies_necessary');
-        $f->label = $this->_('Cookies: Necessary');
-        $f->description = $this->_("Cookie name | Domain | Description | Duration");
-        $f->columnWidth = 100;
-        $cookieFieldset->add($f);
-
-        $f = $this->modules->get('InputfieldTextarea');
-        $f->attr('name', 'cookies_statistics');
-        $f->label = $this->_('Cookies: Statistics');
-        $f->description = $this->_("Cookie name | Domain | Description | Duration");
-        $f->columnWidth = 100;
-        $cookieFieldset->add($f);
-
-        $f = $this->modules->get('InputfieldTextarea');
-        $f->attr('name', 'cookies_external_media');
-        $f->label = $this->_('Cookies: External Media');
-        $f->description = $this->_("Cookie name | Domain | Description | Duration");
-        $f->columnWidth = 100;
-        $cookieFieldset->add($f);
-
-        $f = $this->modules->get('InputfieldTextarea');
-        $f->attr('name', 'cookies_marketing');
-        $f->label = $this->_('Cookies: Marketing');
-        $f->description = $this->_("Cookie name | Domain | Description | Duration");
-        $f->columnWidth = 100;
-        $cookieFieldset->add($f);
 
         return $inputfields;
     }
