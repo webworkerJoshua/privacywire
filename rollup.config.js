f29bfe7b3eed3feba7f93c1b4cc6d6135a4acf62
blaueQuelle <jk@blauequelle.de>
2020-02-16 12:16:33 +0100

    Switched from Laravel Mix to Rollup.js for asset management (smaller footprint)

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/js/PrivacyWire.js',
+  output: {
+    file: 'js/PrivacyWire.js'
+  },
+  plugins: [
+    resolve(),
+    postcss({
+      minimize: true
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    commonjs(),
+    production && terser()
+  ],
+};
--- webpack.mix.js
+++
@@ -1,9 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.options({
-  postCss: [require('autoprefixer')]
-});
-mix.disableNotifications();
-mix.js('src/js/PrivacyWire.js', 'js/PrivacyWire.js')
-  .sass('src/scss/PrivacyWire.scss', 'css/PrivacyWire.css');
-
