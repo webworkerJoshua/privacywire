72bd1442930d5bd4e1c9ac40ec3f0f6c6a78b1b0
Teppo Koivula <teppo.koivula@avoine.fi>
2020-11-19 17:49:21 +0200

    Add support for replacing embedded media with TextformatterPrivacyWire

--- TextformatterPrivacyWireConfig.php
+++ TextformatterPrivacyWireConfig.php
@@ -7,6 +7,7 @@ class TextformatterPrivacyWireConfig extends ModuleConfig
         return [
             'open_tag' => '[[',
             'close_tag' => ']]',
+            'video_category' => null,
         ];
     }
 
@@ -27,6 +28,26 @@ class TextformatterPrivacyWireConfig extends ModuleConfig
         $f->label = $this->_('Close Tag');
         $f->columnWidth = 50;
         $inputfields->add($f);
+
+        // embedded media
+        $fs = $this->modules->get('InputfieldFieldset');
+        $fs->attr('name', 'embedded_media');
+        $fs->label = $this->_('Embedded media');
+        $inputfields->add($fs);
+
+        // category for embedded videos (optional)
+        $f = $this->modules->get('InputfieldSelect');
+        $f->attr('name', 'video_category');
+        $f->label = $this->_('Category for embedded videos');
+        $f->description = $this->_('If you select a category here, users will need to accept said cookie category before embedded video elements are displayed. Leave empty to disable this feature.');
+        $f->notes = $this->_('Recommended if you\'re using [TextformatterVideoEmbed](https://processwire.com/modules/textformatter-video-embed/). Note that TextformatterPrivacyWire need to run after TextformatterVideoEmbed for this to work.');
+        $f->columnWidth = 50;
+        $f->options = [
+            "statistics" => $this->_("Statistics"),
+            'marketing' => $this->_("Marketing"),
+            'external_media' => $this->_("External Media")
+        ];
+        $fs->add($f);
 
         return $inputfields;
     }
