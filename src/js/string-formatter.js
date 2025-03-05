3c58bf1e98902d1408a4ab7616d907a9c837f48d
webworkerJoshua <mail@joshuahennig.de>
2025-03-05 09:32:27 +0100

    `biome` lint & format

--- src/js/string-formatter.js
+++ src/js/string-formatter.js
@@ -1,20 +1,21 @@
 // String formatter to output opt-in message of disabled elements
 // source: https://stackoverflow.com/a/18234317
-String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
-    function () {
-        "use strict";
-        var str = this.toString();
-        if (arguments.length) {
-            var t = typeof arguments[0];
-            var key;
-            var args = ("string" === t || "number" === t) ?
-                Array.prototype.slice.call(arguments)
-                : arguments[0];
+String.prototype.formatUnicorn =
+	String.prototype.formatUnicorn ||
+	function () {
+		var str = this.toString();
+		if (arguments.length) {
+			var t = typeof arguments[0];
+			var key;
+			var args =
+				"string" === t || "number" === t
+					? Array.prototype.slice.call(arguments)
+					: arguments[0];
 
-            for (key in args) {
-                str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
-            }
-        }
+			for (key in args) {
+				str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
+			}
+		}
 
-        return str;
-    };
+		return str;
+	};
